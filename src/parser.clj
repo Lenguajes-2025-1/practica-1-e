f6839d199a63bfde8193812eed04cf8be2df29c7
axellank <axelsandoval@ciencias.unam.mx>
2024-10-11 11:54:07 -0600

    wawa2

--- src/parser.clj
+++ src/parser.clj
@@ -1,17 +1,17 @@
 (ns parser
-  (:require [grammars :refer :all]))
+  (:require [grammars :as wae]))
 
 (defn parser-AE
   "Convierte una expresión aritmética en una instancia de WAE."
   [exp]
   (cond
-    (number? exp) (numG exp)
-    (symbol? exp) (idG exp)
+    (number? exp) (wae/numG exp)
+    (symbol? exp) (wae/idG exp)
     (list? exp)
     (let [op (first exp)]
       (cond
-        (= op '+) (addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
-        (= op '-) (subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+        (= op '+) (wae/addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+        (= op '-) (wae/subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
         :else (throw (IllegalArgumentException. (str "Operación no soportada: " op)))))
     :else (throw (IllegalArgumentException. (str "Expresión inválida: " exp)))))
 
@@ -19,15 +19,15 @@
   "Convierte una expresión con variables (WAE) en una instancia de WAE."
   [exp]
   (cond
-    (number? exp) (numG exp)
-    (symbol? exp) (idG exp)
+    (number? exp) (wae/numG exp)
+    (symbol? exp) (wae/idG exp)
     (list? exp)
     (let [op (first exp)]
       (cond
-        (= op '+) (addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
-        (= op '-) (subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        (= op '+) (wae/addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        (= op '-) (wae/subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
         (= op 'with) (let [[id val] (second exp)
                            body (nth exp 2)]
-                       (withG (bindings id (parser-WAE val)) (parser-WAE body)))
+                       (wae/withG (wae/bindings id (parser-WAE val)) (parser-WAE body)))
         :else (throw (IllegalArgumentException. (str "Operación no soportada: " op)))))
     :else (throw (IllegalArgumentException. (str "Expresión inválida: " exp)))))
