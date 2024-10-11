0afa14246c59f833617cd5a27544b963493ad944
axellank <axelsandoval@ciencias.unam.mx>
2024-10-11 11:48:23 -0600

    wawa

--- src/interp.clj
+++ src/interp.clj
@@ -1,1 +1,20 @@
 ; Continuara...
+(ns interp
+  (:require [grammars :refer :all]))
+
+(defn lookup [id bindings]
+  (let [found (first (filter #(= (:id %) id) bindings))]
+    (if found
+      (:value found)
+      (throw (IllegalArgumentException. (str "Variable no definida: " id))))))
+
+(defn interp [exp env]
+  (cond
+    (instance? NumG exp) (:n exp)
+    (instance? IdG exp) (lookup (:i exp) env)
+    (instance? AddG exp) (+ (interp (:izq exp) env) (interp (:der exp) env))
+    (instance? SubG exp) (- (interp (:izq exp) env) (interp (:der exp) env))
+    (instance? WithG exp) 
+    (let [binding (:assign exp)]
+      (interp (:body exp) (cons (bindings (:id binding) (interp (:value binding) env)) env)))
+    :else (throw (IllegalArgumentException. (str "Expresión desconocida: " exp)))))
--- src/parser.clj
+++ src/parser.clj
@@ -1,12 +1,33 @@
 (ns parser
-  (:require [grammars :as wae]))
+  (:require [grammars :refer :all]))
 
 (defn parser-AE
-    "Por implementar"
-    [exp]
-    ())
+  "Convierte una expresión aritmética en una instancia de WAE."
+  [exp]
+  (cond
+    (number? exp) (numG exp)
+    (symbol? exp) (idG exp)
+    (list? exp)
+    (let [op (first exp)]
+      (cond
+        (= op '+) (addG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+        (= op '-) (subG (parser-AE (second exp)) (parser-AE (nth exp 2)))
+        :else (throw (IllegalArgumentException. (str "Operación no soportada: " op)))))
+    :else (throw (IllegalArgumentException. (str "Expresión inválida: " exp)))))
 
 (defn parser-WAE
-    "Por implemntar"
-    [exp]
-    ())
+  "Convierte una expresión con variables (WAE) en una instancia de WAE."
+  [exp]
+  (cond
+    (number? exp) (numG exp)
+    (symbol? exp) (idG exp)
+    (list? exp)
+    (let [op (first exp)]
+      (cond
+        (= op '+) (addG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        (= op '-) (subG (parser-WAE (second exp)) (parser-WAE (nth exp 2)))
+        (= op 'with) (let [[id val] (second exp)
+                           body (nth exp 2)]
+                       (withG (bindings id (parser-WAE val)) (parser-WAE body)))
+        :else (throw (IllegalArgumentException. (str "Operación no soportada: " op)))))
+    :else (throw (IllegalArgumentException. (str "Expresión inválida: " exp)))))
